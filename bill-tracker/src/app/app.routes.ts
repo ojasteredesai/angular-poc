ab7b55c2cd5bf6b9bffda57a7f319ab4053f7e17
ojas teredesai <ojasteredesai@gmail.com>
2025-12-26 21:25:55 +0530

    routing implemented

--- bill-tracker/src/app/app.routes.ts
+++ bill-tracker/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Timesheet } from './timesheet/timesheet';
+import { Invoice } from './invoice/invoice';
+import { Expense } from './expense/expense';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'timesheet', component: Timesheet },
+  { path: 'invoice', component: Invoice },
+  { path: 'expenses', component: Expense },
+  { path: '**', redirectTo: '/home' }
+];
